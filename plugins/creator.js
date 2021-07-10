49b4d774f5e9a66e08505ea9c7a099a898bceef7
yanzzbot <77144968+yanzzbot@users.noreply.github.com>
2021-07-10 13:34:17 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281803522862', 'My Owner BOT', m)
+  this.sendContact(m.chat, '6281906103554', 'My Owner BOT', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
